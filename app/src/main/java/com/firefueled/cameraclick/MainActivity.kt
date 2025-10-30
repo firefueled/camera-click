d4126f9a8e3ee0965696325ac45d77d8fe9491ae
pablo <pablo@pablo.com>
2025-10-30 17:45:52 -0300

    Add main button and first run

--- app/src/main/java/com/firefueled/cameraclick/MainActivity.kt
+++ app/src/main/java/com/firefueled/cameraclick/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.firefueled.cameraclick
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val cameraButton: ImageButton = findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener {
+            Toast.makeText(this, "Camera clicked!", Toast.LENGTH_SHORT).show()
         }
     }
 }
